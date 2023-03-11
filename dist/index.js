bb5ec4919669701897d3d54e142c5299b5af5e35
afekTheMiniLearner <afeksa2000@gmail.com>
2023-03-11 22:34:24 +0200

    .

--- dist/index.js
+++ dist/index.js
@@ -9,23 +9,4 @@ Object.defineProperty(exports, "Countdown", {
     return _components.Countdown;
   }
 });
-var _react = _interopRequireDefault(require("react"));
-var _reactDom = _interopRequireDefault(require("react-dom"));
-require("./index.css");
-var _App = _interopRequireDefault(require("./App"));
-var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
 var _components = require("./components/");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-/* Hidden to prevent multiple calling ReactDOMClient.createRoot() 
-in case someone using this as NPM package
-unhide if you want to run the app ! 
-*/
-
-var root = document.getElementById("root");
-_reactDom.default.render( /*#__PURE__*/_react.default.createElement(_App.default, null), root);
-(0, _reportWebVitals.default)();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-(0, _reportWebVitals.default)();
