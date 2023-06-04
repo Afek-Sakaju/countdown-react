2398b134fbdf14154758848ba7c55b11f5e58fc2
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-04 18:12:27 +0300

    deleted usage in index except for the export of the countdown

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-// export { Countdown } from "./components";
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import App from "./App";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+export { Countdown } from "./components";
